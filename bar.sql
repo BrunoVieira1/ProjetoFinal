7b9e45c297efb331d86dcef2322d24ffad8671b0
Bruno Vieira Correa <bruno345viera@gmail.com>
2024-09-17 13:37:50 -0300

    sql

--- bar.sql
+++ bar.sql
@@ -1,23 +1,23 @@
 -- phpMyAdmin SQL Dump
--- version 3.4.9
+-- version 4.5.5.1
 -- http://www.phpmyadmin.net
 --
--- Servidor: 127.0.0.1
--- Tempo de Geração: 04/09/2024 às 22h13min
--- Versão do Servidor: 5.5.20
--- Versão do PHP: 5.3.9
+-- Host: 127.0.0.1
+-- Generation Time: 17-Set-2024 às 18:37
+-- Versão do servidor: 5.7.11
+-- PHP Version: 5.6.19
 
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
--- Banco de Dados: `bar`
+-- Database: `bar`
 --
 
 -- --------------------------------------------------------
@@ -26,13 +26,11 @@ SET time_zone = "+00:00";
 -- Estrutura da tabela `brand`
 --
 
-CREATE TABLE IF NOT EXISTS `brand` (
-  `id` int(5) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `brand` (
+  `id` int(5) NOT NULL,
   `name` varchar(100) NOT NULL,
-  `idRequester` int(5) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `idRequester` (`idRequester`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+  `idRequester` int(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Extraindo dados da tabela `brand`
@@ -48,14 +46,12 @@ INSERT INTO `brand` (`id`, `name`, `idRequester`) VALUES
 -- Estrutura da tabela `debtor`
 --
 
-CREATE TABLE IF NOT EXISTS `debtor` (
+CREATE TABLE `debtor` (
   `id` int(11) NOT NULL,
   `name` int(11) NOT NULL,
   `price` float(8,2) NOT NULL,
   `date` date NOT NULL,
-  `idRequester` int(5) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `idRequester` (`idRequester`)
+  `idRequester` int(5) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -64,15 +60,13 @@ CREATE TABLE IF NOT EXISTS `debtor` (
 -- Estrutura da tabela `expenses`
 --
 
-CREATE TABLE IF NOT EXISTS `expenses` (
-  `id` int(5) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `expenses` (
+  `id` int(5) NOT NULL,
   `reason` varchar(100) NOT NULL,
   `price` float(8,2) NOT NULL,
   `date` date NOT NULL,
-  `idRequester` int(5) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `idRequester` (`idRequester`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+  `idRequester` int(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -80,17 +74,14 @@ CREATE TABLE IF NOT EXISTS `expenses` (
 -- Estrutura da tabela `orders`
 --
 
-CREATE TABLE IF NOT EXISTS `orders` (
-  `id` int(5) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `orders` (
+  `id` int(5) NOT NULL,
   `idProduct` int(5) NOT NULL,
   `qtt` int(4) NOT NULL,
   `total` float(8,2) NOT NULL,
   `date` date NOT NULL,
-  `idRequester` int(5) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `idProduct` (`idProduct`),
-  UNIQUE KEY `idRequester` (`idRequester`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+  `idRequester` int(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -98,11 +89,10 @@ CREATE TABLE IF NOT EXISTS `orders` (
 -- Estrutura da tabela `position`
 --
 
-CREATE TABLE IF NOT EXISTS `position` (
-  `id` int(5) NOT NULL AUTO_INCREMENT,
-  `name` varchar(100) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+CREATE TABLE `position` (
+  `id` int(5) NOT NULL,
+  `name` varchar(100) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
 -- Extraindo dados da tabela `position`
@@ -118,23 +108,23 @@ INSERT INTO `position` (`id`, `name`) VALUES
 -- Estrutura da tabela `product`
 --
 
-CREATE TABLE IF NOT EXISTS `product` (
-  `id` int(5) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `product` (
+  `id` int(5) NOT NULL,
   `name` varchar(100) NOT NULL,
   `idBrand` int(5) NOT NULL,
   `type` varchar(100) NOT NULL,
   `price` float(8,2) NOT NULL,
-  `idRequester` int(5) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `idRequester` (`idRequester`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+  `idRequester` int(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Extraindo dados da tabela `product`
 --
 
 INSERT INTO `product` (`id`, `name`, `idBrand`, `type`, `price`, `idRequester`) VALUES
-(1, 'Brahma Lata 269Ml', 3, 'Cerveja Lata', 2.79, 1);
+(1, 'Brahma Lata 269Ml', 3, 'Cerveja Lata', 2.79, 1),
+(2, 'Pilsner Duplo Malte', 3, 'Lata', 2.89, 1),
+(3, 'teste', 1, 'Lata', 2.12, 1);
 
 -- --------------------------------------------------------
 
@@ -142,24 +132,22 @@ INSERT INTO `product` (`id`, `name`, `idBrand`, `type`, `price`, `idRequester`) 
 -- Estrutura da tabela `stock`
 --
 
-CREATE TABLE IF NOT EXISTS `stock` (
-  `id` int(5) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `stock` (
+  `id` int(5) NOT NULL,
   `idProduct` int(5) NOT NULL,
   `minStock` int(4) NOT NULL,
   `maxStock` int(4) NOT NULL,
   `qtt` int(4) NOT NULL,
-  `idRequester` int(5) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `idProduct` (`idProduct`),
-  UNIQUE KEY `idRequester` (`idRequester`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+  `idRequester` int(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Extraindo dados da tabela `stock`
 --
 
 INSERT INTO `stock` (`id`, `idProduct`, `minStock`, `maxStock`, `qtt`, `idRequester`) VALUES
-(1, 1, 10, 1000, 100, 1);
+(1, 1, 10, 1000, 112, 1),
+(8, 2, 10, 1000, 12, 1);
 
 -- --------------------------------------------------------
 
@@ -167,16 +155,20 @@ INSERT INTO `stock` (`id`, `idProduct`, `minStock`, `maxStock`, `qtt`, `idReques
 -- Estrutura da tabela `stockin`
 --
 
-CREATE TABLE IF NOT EXISTS `stockin` (
-  `id` int(5) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `stockin` (
+  `id` int(5) NOT NULL,
   `idProduct` int(5) NOT NULL,
   `qtt` int(4) NOT NULL,
   `date` date NOT NULL,
-  `idRequester` int(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `idProduct` (`idProduct`),
-  UNIQUE KEY `idRequester` (`idRequester`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+  `idRequester` int(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `stockin`
+--
+
+INSERT INTO `stockin` (`id`, `idProduct`, `qtt`, `date`, `idRequester`) VALUES
+(1, 2, 12, '2024-09-17', 1);
 
 -- --------------------------------------------------------
 
@@ -184,16 +176,20 @@ CREATE TABLE IF NOT EXISTS `stockin` (
 -- Estrutura da tabela `stockout`
 --
 
-CREATE TABLE IF NOT EXISTS `stockout` (
-  `id` int(5) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `stockout` (
+  `id` int(5) NOT NULL,
   `idProduct` int(5) NOT NULL,
   `qtt` int(4) NOT NULL,
   `date` date NOT NULL,
-  `idRequester` int(5) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `idProduct` (`idProduct`),
-  UNIQUE KEY `idRequester` (`idRequester`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+  `idRequester` int(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `stockout`
+--
+
+INSERT INTO `stockout` (`id`, `idProduct`, `qtt`, `date`, `idRequester`) VALUES
+(1, 2, 12, '2024-09-10', 1);
 
 -- --------------------------------------------------------
 
@@ -201,17 +197,14 @@ CREATE TABLE IF NOT EXISTS `stockout` (
 -- Estrutura da tabela `user`
 --
 
-CREATE TABLE IF NOT EXISTS `user` (
-  `id` int(5) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `user` (
+  `id` int(5) NOT NULL,
   `name` varchar(100) NOT NULL,
   `login` varchar(100) NOT NULL,
   `password` varchar(100) NOT NULL,
   `email` varchar(100) NOT NULL,
-  `idPosition` int(5) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `name` (`name`),
-  KEY `idPosition` (`idPosition`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+  `idPosition` int(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Extraindo dados da tabela `user`
@@ -221,26 +214,171 @@ INSERT INTO `user` (`id`, `name`, `login`, `password`, `email`, `idPosition`) VA
 (1, 'as', 'ass', 'ass', 'ass', 1);
 
 --
--- Restrições para as tabelas dumpadas
+-- Indexes for dumped tables
 --
 
 --
--- Restrições para a tabela `brand`
+-- Indexes for table `brand`
+--
+ALTER TABLE `brand`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `idRequester` (`idRequester`);
+
+--
+-- Indexes for table `debtor`
+--
+ALTER TABLE `debtor`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `idRequester` (`idRequester`);
+
+--
+-- Indexes for table `expenses`
+--
+ALTER TABLE `expenses`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `idRequester` (`idRequester`);
+
+--
+-- Indexes for table `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `idProduct` (`idProduct`),
+  ADD UNIQUE KEY `idRequester` (`idRequester`);
+
+--
+-- Indexes for table `position`
+--
+ALTER TABLE `position`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `product`
+--
+ALTER TABLE `product`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `idRequester` (`idRequester`);
+
+--
+-- Indexes for table `stock`
+--
+ALTER TABLE `stock`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `idProduct` (`idProduct`),
+  ADD KEY `idRequester` (`idRequester`);
+
+--
+-- Indexes for table `stockin`
+--
+ALTER TABLE `stockin`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `idProduct` (`idProduct`),
+  ADD KEY `idRequester` (`idRequester`);
+
+--
+-- Indexes for table `stockout`
+--
+ALTER TABLE `stockout`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `idProduct` (`idProduct`),
+  ADD KEY `idRequester` (`idRequester`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `name` (`name`),
+  ADD KEY `idPosition` (`idPosition`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `brand`
+--
+ALTER TABLE `brand`
+  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT for table `expenses`
+--
+ALTER TABLE `expenses`
+  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `position`
+--
+ALTER TABLE `position`
+  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT for table `product`
+--
+ALTER TABLE `product`
+  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT for table `stock`
+--
+ALTER TABLE `stock`
+  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+--
+-- AUTO_INCREMENT for table `stockin`
+--
+ALTER TABLE `stockin`
+  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for table `stockout`
+--
+ALTER TABLE `stockout`
+  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Limitadores para a tabela `brand`
 --
 ALTER TABLE `brand`
   ADD CONSTRAINT `brand_ibfk_1` FOREIGN KEY (`idRequester`) REFERENCES `user` (`id`);
 
 --
--- Restrições para a tabela `debtor`
+-- Limitadores para a tabela `debtor`
 --
 ALTER TABLE `debtor`
   ADD CONSTRAINT `debtor_ibfk_1` FOREIGN KEY (`idRequester`) REFERENCES `user` (`id`);
 
 --
--- Restrições para a tabela `product`
+-- Limitadores para a tabela `product`
 --
 ALTER TABLE `product`
   ADD CONSTRAINT `product_ibfk_1` FOREIGN KEY (`idRequester`) REFERENCES `user` (`id`);
+
+--
+-- Limitadores para a tabela `stock`
+--
+ALTER TABLE `stock`
+  ADD CONSTRAINT `stock_ibfk_1` FOREIGN KEY (`idRequester`) REFERENCES `user` (`id`);
+
+--
+-- Limitadores para a tabela `stockin`
+--
+ALTER TABLE `stockin`
+  ADD CONSTRAINT `stockin_ibfk_1` FOREIGN KEY (`idRequester`) REFERENCES `user` (`id`);
+
+--
+-- Limitadores para a tabela `stockout`
+--
+ALTER TABLE `stockout`
+  ADD CONSTRAINT `stockout_ibfk_1` FOREIGN KEY (`idRequester`) REFERENCES `user` (`id`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
