cb10c44ca27fc70e4da3cad370a0c3b451011574
Bruno Vieira Correa <bruno345viera@gmail.com>
2024-09-26 17:19:15 -0300

    um pouco da tela de grafico

--- Back/routes/stockRoutes.py
+++ Back/routes/stockRoutes.py
@@ -1,12 +1,36 @@
 from controllers.stockController import stock_controller
 from models.stock import Stock
+from models.product import Product
+from database.db import db
 
 def min_stock():
-    data = Stock.query.filter(Stock.minStock >= Stock.qtt).all()
-    data2 = Stock.query.filter(Stock.maxStock <= Stock.qtt).all()
-    print(data[0])
-    print(data2)
-    return [[data.to_dict() for data in data], [data2.to_dict() for data2 in data2]]
+    data = db.session.query(Stock, Product).filter(Stock.minStock >= Stock.qtt).join(Product, Product.id == Stock.idProduct).all()
+    data2 = db.session.query(Stock, Product).filter(Stock.maxStock <= Stock.qtt).join(Product, Product.id == Stock.idProduct).all()
+    dados = [
+                {
+                    'id' : stock.id,
+                    'idProduct' : stock.idProduct,
+                    'minStock' : stock.minStock,
+                    'maxStock' : stock.maxStock,
+                    'qtt' : stock.qtt,
+                    'name' : product.name,
+                }
+                for stock, product in data
+            ]
+    dados2 = [
+                {
+                    'id' : stock.id,
+                    'idProduct' : stock.idProduct,
+                    'minStock' : stock.minStock,
+                    'maxStock' : stock.maxStock,
+                    'qtt' : stock.qtt,
+                    'name' : product.name,
+                }
+                for stock, product in data2
+            ]
+    print(dados)
+    print(dados2)
+    return [dados, dados2]
 def stock_routes(app):
     app.route('/stock', methods=['POST', 'GET', 'PUT', 'DELETE'])(stock_controller)
     app.route('/stock/min', methods=['GET'])(min_stock)
--- Front/src/screens/graphs/graphs.tsx
+++ Front/src/screens/graphs/graphs.tsx
@@ -32,11 +32,82 @@ function Graphs() {
   return (
     <div className="flex justify-center items-center w-full flex-col gap-4">
       <Logo />
-      <div className="flex flex-1">
+      <div className="flex">
         <Graph />
         <Graph />
       </div>
-      <div></div>
+      <div className="flex gap-2">
+        <div className="border border-black rounded">
+          <h1 className="border-b border-black font-bold text-red-700 text-center">
+            Produtos com baixo estoque
+          </h1>
+          <div>
+            {stockmin.map((sm) => (
+              <table key={sm.id}>
+                <tr className="border-b border-black bg-red-300">
+                  <th className="border-r border-black px-2 text-left">ID</th>
+                  <th className="border-r border-black px-2 text-left">Nome</th>
+                  <th className="border-r border-black px-2 text-left">
+                    Quantidade
+                  </th>
+                  <th className=" border-black px-2 text-left">
+                    Quantidade Minima
+                  </th>
+                </tr>
+                <tr className="bg-red-100">
+                  <td className="border-r border-black px-2 text-left">
+                    {sm.id}
+                  </td>
+                  <td className="border-r border-black px-2 text-left">
+                    {sm.name}
+                  </td>
+                  <td className="border-r border-black px-2 text-left">
+                    {sm.qtt}
+                  </td>
+                  <td className=" border-black px-2 text-left">
+                    {sm.minStock}
+                  </td>
+                </tr>
+              </table>
+            ))}
+          </div>
+        </div>
+        <div className="border border-black rounded">
+          <h1 className="border-b border-black font-bold text-green-700 text-center">
+            Produtos com alto estoque
+          </h1>
+          <div>
+            {stockmax.map((sm) => (
+              <table key={sm.id}>
+                <tr className="border-b border-black bg-green-300">
+                  <th className="border-r border-black px-2 text-left">ID</th>
+                  <th className="border-r border-black px-2 text-left">Nome</th>
+                  <th className="border-r border-black px-2 text-left">
+                    Quantidade
+                  </th>
+                  <th className=" border-black px-2 text-left">
+                    Quantidade Maxima
+                  </th>
+                </tr>
+                <tr className="bg-green-100">
+                  <td className="border-r border-black px-2 text-left">
+                    {sm.id}
+                  </td>
+                  <td className="border-r border-black px-2 text-left">
+                    {sm.name}
+                  </td>
+                  <td className="border-r border-black px-2 text-left">
+                    {sm.qtt}
+                  </td>
+                  <td className=" border-black px-2 text-left">
+                    {sm.maxStock}
+                  </td>
+                </tr>
+              </table>
+            ))}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
