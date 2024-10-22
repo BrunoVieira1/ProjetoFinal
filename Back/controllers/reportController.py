7ecee4f6a4204e8378fbf65c5a8a260426096b55
Bruno Vieira Correa <bruno345viera@gmail.com>
2024-10-22 17:01:28 -0300

    mais relatorio

--- Back/controllers/reportController.py
+++ Back/controllers/reportController.py
@@ -1,3 +1,4 @@
+from collections import Counter
 from models.user import User
 from models.stockin import StockIn
 from models.stockout import StockOut
@@ -7,34 +8,79 @@ from reportlab.lib.pagesizes import A4
 from datetime import datetime, timedelta, date
 from database.db import db
 
-def download_pdf():
+def datasession(stock, cnv, vendas):
     hoje = datetime.now()
     trinta_dias_atras = hoje - timedelta(days=30)
-
-    queryStockOut = db.session.query(StockOut, Product).join(Product, Product.id == StockOut.idProduct).all()
+    column = 800
+    if vendas == "Vendas":
+        row = 350
+    else:
+        row = 80
+    queryStockOut = db.session.query(stock, Product).join(Product, Product.id == stock.idProduct).all()
+    queryStock = db.session.query(stock).all()
+    
     data2 = [
                 {
-                    'id' : stockin.id,
-                    'idProduct' : stockin.idProduct,
-                    'qtt' : stockin.qtt,
-                    'date' : stockin.date.strftime('%Y-%m-%d'),
-                    'name' : product.name
+                    'id' : stock.id,
+                    'idProduct' : stock.idProduct,
+                    'qtt' : stock.qtt,
+                    'date' : stock.date.strftime('%Y-%m-%d'),
+                    'name' : product.name,
+                    'price' : product.price
                 }
-                for stockin, product in queryStockOut
+                for stock, product in queryStockOut
             ]
+
+    numeros = [obj["idProduct"] for obj in data2]
+
+    contador = Counter(numeros)
+    print(contador)
+
+    numeros_iguais = {num: count for num, count in contador.items() if count > 0}
+    seila = []
+    seila2 = []
+    xd = 0
+    xd2 = 0
+    for numero, quantidade in numeros_iguais.items():
+        for i in data2:
+            if i['idProduct'] == numero:
+                xd += round(float(i['qtt'] * i['price']), 2)
+                xd3 = i['name']
+
+        xd2 += xd
+        print(xd)
+        seila.append(xd2)
+        seila2.append(xd3)
+        xd2 = 0
+        xd = 0
+    junto = []
+    for i in range(len(seila)):
+        xd4 = [seila2[i] , seila[i]]
+        junto.append(xd4) 
+
+    print(seila)
+    print(seila2)
+    print(junto)
+
+    datastock = [stock for stock in data2 if datetime.strptime(stock['date'], '%Y-%m-%d') >= trinta_dias_atras]
+    datastock = sorted(datastock, key=lambda x: x['date'])
+    cnv.drawString(row, column, f"{vendas}: ordem data")
+    column -= 20 
+    row -= 60
+    for i in datastock:
+        cnv.drawString(row, column, f"{i['name']} - Quantidade: {i['qtt']} - Data: {i['date']}")
+        column -= 20
+
+def download_pdf():
     
-    dataStockOut = [stockout for stockout in data2 if datetime.strptime(stockout['date'], '%Y-%m-%d') >= trinta_dias_atras]
-    dataStockOut = sorted(dataStockOut, key=lambda x: x['date'])
-    print(dataStockOut[0]["id"])
+    
     cnv = canvas.Canvas("pdf.pdf", pagesize=A4)
+    cnv.setFont("Helvetica", 8)
     queryUser = User.query.all()
     dataUser = [usuario.to_dict() for usuario in queryUser][0]
-    cnv.drawString(50, 800, dataUser['name'])
-    cnv.drawString(350, 800, "Vendas: ordem data")
-    row = 780
-    for i in dataStockOut:
-        cnv.drawString(230, row, f"{i['name']} - Quantidade: {i['qtt']} - Data: {i['date']}")
-        row -= 30
+    
+    datasession(StockOut, cnv, "Vendas")
+    datasession(StockIn, cnv, "Compras")
     cnv.save()
 
     return "true"
